74f9f2018b175fdaef34120e7b5164f038883f23
Mohammad Istanbouly <mohammadistanbouly@Mohammads-MacBook-Pro.local>
2018-06-13 11:46:41 +0300

    fix the validator trait and add bunch of validation methods also add comments for everything in the package

--- src/ITWOC/ITWOC.php
+++ src/ITWOC/ITWOC.php
@@ -10,16 +10,37 @@ use Monolog\Formatter\LineFormatter;
 use SoapClient;
 
 class ITWOC {
-	// use ITWOCValidatorTrait;
+	use ITWOCValidatorTrait;
 
+    /**
+     * $_client will store instance of the SoapClient so we can use the soap services
+     * @var SoapClient
+     */
 	protected $_client;
 
+    /**
+     * $_wsdlUrl the url of wsdl file that defines the soap services so we can use them it
+     * will be loaded from configuration file
+     * @var String
+     */
 	protected $_wsdlUrl;
 
+    /**
+     * $_acquirer will store array of the Acqirer data
+     * @var Array
+     */
 	protected $_acquirer;
 
+    /**
+     * $_logPath will store the log file path for this package and will be loaded from the configuration files
+     * @var String
+     */
 	protected $_logPath;
 
+
+    /**
+     * __construct will initialize the package important data and setup some helpers like Logger & SoapClient
+     */
 	public function __construct(){
 		$this->init();
 		$this->_logger = new Logger('ITWOC');
@@ -29,12 +50,15 @@ class ITWOC {
 		// the default output format is "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n"
 		$output = "%datetime% > %level_name% > %message% %context% %extra%\n";
 
-		$formatter =  new LineFormatter($output,$dateFormat);
+		$formatter =  new LineFormatter($output,$dateFormat , true);
 		$stream = new StreamHandler($this->_logPath, Logger::DEBUG);
 		$stream->setFormatter($formatter);
 		$this->_logger->pushHandler($stream);
 	}
 
+    /**
+     * init will load configurations and set up the soap client
+     */
 	protected function init(){
 		//load data from configuration file
 		$this->_wsdlUrl = config('itwoc.wsdl_file');
@@ -45,41 +69,122 @@ class ITWOC {
 		$this->_client = new SoapClient( $this->_wsdlUrl ,array("trace" => 1, "exception" => 0));
 	}
 
-	public function addCard(array $data = []) : string{
-        $this->_acquirer['Acquirer']['ARN'] = substr(hash('sha512' , microtime() . uniqid() . str_random(100) ) , 0 , 20);
-        $data = $this->_acquirer + $data;
+    /**
+     * getAcquirer will return the Acquirer along with a unique generated ARN
+     * @return Array [array of acquirer details]
+     */
+    protected function getAcquirer(){
+        $acquirer = $this->_acquirer;
+        $acquirer['Acquirer']['ARN'] = substr(hash('sha512' , microtime() . uniqid() . str_random(100) ) , 0 , 20);
+        return $acquirer;
+    }
 
-		try{
-            $this->logInfo(json_encode($data));
-            $res = $this->_client->__Call("AddCard" , [$data]);
+	public function addCard(array $data = []) {
+        if($this->validateAddCardAction($data)){
+            $acquirer = $this->getAcquirer();
+            $data = $acquirer + $data;
+
+            try{
+                $this->logInfo(json_encode($data));
+                $res = $this->_client->__Call("AddCard" , [$data]);
+
+                $response = [
+                    'response' => [
+                        'response_code' => $res->ResponseCode,
+                        'Response_desc' => $res->ResponseDesc,
+                        'reference_id' => $res->ReferenceID
+                    ]
+                ];
 
 
-            $response = [
-                'response' => [
-                    'response_code' => $res->ResponseCode,
-                    'Response_desc' => $res->ResponseDesc,
-                    'response_id' => $res->ReferenceID
-                ]
-            ];
+                $this->logInfo(json_encode($response));
 
-            $this->logInfo(json_encode($response));
+                return $acquirer['Acquirer']['ARN'];
 
-            return $this->_acquirer['Acquirer']['ARN'];
+            }catch(\SoapException $e){
+                $response = [
+                    'response' => [
+                        'response_code' => $e->getCode(),
+                        'Response_desc' => $e->getMessage(),
+                    ]
+                ];
 
-        }catch(\SoapException $e){
-            $response = [
-                'response' => [
-                    'response_code' => $e->getCode(),
-                    'Response_desc' => $e->getMessage(),
-                ]
-            ];
-
-            $this->logError(json_encode($response));
-
-            return 'error';
+                $this->logError(json_encode($response));
+            }
         }
 	}
 
+
+    public function loadCard(array $data = []){
+        if($this->validateLoadCardAction($data)){
+            $acquirer = $this->getAcquirer();
+            $data = $acquirer + $data;
+
+
+            try{
+                $this->logInfo(json_encode($data));
+                $res = $this->_client->__Call("CreditFunds" , [$data]);
+
+                $response = [
+                    'response' => [
+                        'response_code' => $res->ResponseCode,
+                        'Response_desc' => $res->ResponseDesc,
+                        // 'reference_id' => $res->ReferenceID,
+                    ]
+                ];
+
+                $this->logInfo(json_encode($response));
+
+                return $acquirer['Acquirer']['ARN'];
+
+            }catch(\SoapException $e){
+                $response = [
+                    'response' => [
+                        'response_code' => $e->getCode(),
+                        'Response_desc' => $e->getMessage(),
+                    ]
+                ];
+
+                $this->logError(json_encode($response));
+            }
+        }
+    }
+
+
+
+    public function checkBalance(array $data = []){
+        if($this->validateCheckBalanceAction($data)){
+            $acquirer = $this->getAcquirer();
+            $data = $acquirer + $data;
+
+            try{
+                $this->logInfo(json_encode($data));
+                $res = $this->_client->__Call("balanceInquiry" , [$data]);
+
+                $response = [
+                    'response' => [
+                        'response_code' => $res->ResponseCode,
+                        'Response_desc' => $res->ResponseDesc,
+                        // 'reference_id' => $res->ReferenceID,
+                    ]
+                ];
+
+                $this->logInfo(json_encode($response));
+
+                return $acquirer['Acquirer']['ARN'];
+
+            }catch(\SoapException $e){
+                $response = [
+                    'response' => [
+                        'response_code' => $e->getCode(),
+                        'Response_desc' => $e->getMessage(),
+                    ]
+                ];
+
+                $this->logError(json_encode($response));
+            }
+        }
+    }
 
 
 	//logging functions
--- src/ITWOC/Traits/ITWOCValidatorTrait.php
+++ src/ITWOC/Traits/ITWOCValidatorTrait.php
@@ -1,78 +1,117 @@
-<?php 
+<?php
 
 namespace ITWOC\Traits;
 
 trait ITWOCValidatorTrait {
-	// protected $_addCardAction = [
-	// 	'CardAcceptor' => ['Id' => ''],
-	// 	'Card' => ['StartingNumbers' => ''],
-	// 	'Profile' => [
-	// 			[
-	// 				'FirstName' => '',
-	// 				'LastName' => '',
-	// 				'Email' => '',
-	// 				'CellNumber' => ''
-	// 			],
-	// 		'ApplyFee' => '',
-	// 		]
-	// ];
-	
-	// protected function validateAddCardAction(array $data = [])  {
-	// 	return $this->followsFormat($data , $this->_addCardAction);
-	// 	// return $this->followsFormat($data , $this->_addCardAction);
-	// }
-
-	// protected function followsFormat($data , $format){
-	// 	if(array_keys($data) != array_keys($format))
-	// 		return false;
+    /**
+     * This trait is a helper trait for ITWOC Class and the aim for it is to sanitize data
+     * and check if the called service passed array of it in the same structure that it should
+     * each service should have a protected array that defines it's structure for example
+     * add cardSerivce should have array of structure here called _addCardAction then to check
+     * if the cardService passed array matches the structure of the _addCardAction array we send them
+     * both to the protected function followsFormat that will recursivly traverse both arrays and check
+     * for differences in there structure based on there keys not values
+     */
 
 
-	// 		print_r($data);
-	// 		echo "<hr>";
-	// 		print_r($format);
+    /**
+     * $_addCardAction shows the correct structure for AddCard Service Array
+     * @var Array
+     */
+	protected $_addCardAction = [
+		'CardAcceptor' => ['Id' => ''],
+		'Card' => ['StartingNumbers' => ''],
+		'Profile' => [
+				[
+					'FirstName' => '',
+					'LastName' => '',
+					'Email' => '',
+					'CellNumber' => ''
+				],
+			'ApplyFee' => '',
+			]
+	];
+
+    /**
+     * $_loadCardAction show the correct structure for LoadCard Service Array
+     * @var Array
+     */
+    protected $_loadCardAction = [
+        'CardAcceptor' => ['Id' => '' ],
+        'Card' => ['ReferenceID' => ''],
+        'FundingCard' => ['Number' => ''],
+        'ApplyFee' => '',
+        'Amount' => 0
+    ];
+
+    /**
+     * $_checkBalanceAction show the correct structure for CheckBalance Service Array
+     * @var Array
+     */
+    protected $_checkBalanceAction = [
+        'CardAcceptor' => ['Id' => '' ],
+        'Card' => [ 'ReferenceID' => ''],
+        'ApplyFee' => '',
+    ];
+
+    /**
+     * validateAddCardAction accepts the array to be validated fot AddCard Service
+     * it will pass the array it recieved along with it's correct structure that
+     * it should implement to the followsFormat function
+     * @param  Array  $data [array for AddCard Service]
+     * @return Bool       [whether it follows the format or not]
+     */
+	protected function validateAddCardAction(array $data = [])  {
+		return $this->followsFormat($data , $this->_addCardAction);
+	}
+
+    /**
+     * validateLoadCardAction accepts the array to be validated fot LoadCard Service
+     * it will pass the array it recieved along with it's correct structure that
+     * it should implement to the followsFormat function
+     * @param  Array  $data [array for LoadCard Service]
+     * @return Bool       [whether it follows the format or not]
+     */
+    protected function validateLoadCardAction(array $data = []){
+        return $this->followsFormat($data , $this->_loadCardAction);
+    }
 
 
-	// 	foreach ($data as $key => $value) {
-	// 		if(is_array($value)){
-	// 			$bool = $this->followsFormat($value , $format[$key]);
+    /**
+     * validateCheckBalanceAction accepts the array to be validated fot CheckBalance Service
+     * it will pass the array it recieved along with it's correct structure that
+     * it should implement to the followsFormat function
+     * @param  Array  $data [array for CheckBalance Service]
+     * @return Bool       [whether it follows the format or not]
+     */
+    protected function validateCheckBalanceAction(array $data = []){
+        return $this->followsFormat($data , $this->_checkBalanceAction);
+    }
 
-	// 			if(!$bool)
-	// 				return $bool;
-	// 		}
-	// 	}
 
-	// 	return true;
-	// }
+    /**
+     * followsFormat checks if the passed array follows it's predefined structure
+     * @param  Array $data   [array of data for a particular service]
+     * @param  [Array] $format [array of predefined data structure for a particular service  ]
+     * @return [Bool]         [whether the match or not based on there keys matching recursively]
+     */
+	protected function followsFormat($data , $format){
+		if(array_keys($data) != array_keys($format))
+			return false;
+
+			// print_r($data);
+			// echo "<hr>";
+			// print_r($format);
+
+		foreach ($data as $key => $value) {
+			if(is_array($value)){
+				$bool = $this->followsFormat($value , $format[$key]);
+
+				if(!$bool)
+					return $bool;
+			}
+		}
+
+		return true;
+	}
 }
-
-
-	// protected function followsFormat($data , $format){
-	// 	if(array_keys($data) != array_keys($format))
-	// 		return false;
-
-
-	// 	foreach ($format as $key => $value) {
-	// 		if(is_array($value)  ){
-	// 			if(is_array($data[$key])){
-	// 				$bool = $this->followsFormat($data[$key] , $value);
-	// 				if(!$bool)
-	// 					return $bool;
-	// 			} else {
-	// 				return false; 
-	// 			}
-				
-	// 		}
-	// 	}
-
-	// 	return true;
-	// }
-
-
-
-
-
-
-
-
-
-
